4e0cbc788b821b9d29c65df8ec465d796b333b0a
dzazef <dzazef@gmail.com>
2020-03-29 20:55:15 +0200

    Fix tsp

--- lista1/z2/zad2.py
+++ lista1/z2/zad2.py
@@ -3,14 +3,34 @@ Solving TSP using Tabu Search
 """
 import sys
 import time
+import copy
 
-import numpy as np
-
-tabu_size = int(1e7)
+tabu_size = int(1e9)
 
 
-def gen_first_solution(n):
-    return [0] + list(np.random.permutation([i for i in range(1, n)])) + [0]
+def gen_first_solution(n, dist):
+    """Greedy algorithm to generate first solution"""
+    to_visit = [x for x in range(1, n)]
+    solution = [0]
+    last = 0
+    while len(to_visit) > 0:
+        best_city = to_visit[0]
+        best_dist = dist[last][best_city]
+        for curr_city in to_visit:
+            curr_dist = dist[last][curr_city]
+            if curr_dist < best_dist:
+                best_city = curr_city
+                best_dist = curr_dist
+        solution.append(best_city)
+        to_visit.remove(best_city)
+    solution.append(0)
+    return solution
+
+
+# def gen_first_solution(n, dist):
+#     q = [0] + list(np.random.permutation([i for i in range(1, n)])) + [0]
+#     print(q, find_distance(q, dist))
+#     return q
 
 
 def find_distance(s, dist):
@@ -25,7 +45,7 @@ def gen_neighbourhood(solution):
             idx_y = solution.index(y)
             if x == y:
                 continue
-            new = [e for e in solution]
+            new = copy.deepcopy(solution)
             new[idx_x], new[idx_y] = y, x
             if new not in neighbourhood:
                 neighbourhood.append(new)
@@ -50,23 +70,27 @@ def get_diff(perm1, perm2):
 
 def solve_tsp(t, n, dist):
     tabu = []
-    solution = gen_first_solution(n)
-    best_solution = solution
-    distance = find_distance(solution, dist)
-    best_distance = distance
+    best_solution = gen_first_solution(n, dist)
+    best_distance = find_distance(best_solution, dist)
+    solution = copy.deepcopy(best_solution)
     end_time = time.time() + t
     while time.time() < end_time:
         neighbourhood = gen_neighbourhood(solution)
+        best_neighbour = neighbourhood[0]
+        best_neighbour_dist = find_distance(best_neighbour, dist)
         for neighbour in neighbourhood:
-            first, second = get_diff(neighbour, solution)
+            first, second = get_diff(solution, neighbour)
             if (first, second) not in tabu and (second, first) not in tabu:
-                tabu.append((first, second))
-                solution = neighbour
-                distance = find_distance(neighbour, dist)
-                if distance < best_distance:
-                    best_solution = solution
-                    best_distance = distance
-                break
+                tmp_neighbour_dist = find_distance(neighbour, dist)
+                if tmp_neighbour_dist < best_neighbour_dist:
+                    best_neighbour = neighbour
+                    best_neighbour_dist = tmp_neighbour_dist
+        tabu.append((get_diff(solution, best_neighbour)))
+        solution = best_neighbour
+        distance = best_neighbour_dist
+        if distance < best_distance:
+            best_solution = copy.deepcopy(solution)
+            best_distance = distance
         if len(tabu) >= tabu_size:
             tabu.pop(0)
     return best_solution, best_distance
@@ -80,9 +104,7 @@ def run():
     solution, distance = solve_tsp(t, n, dist_matrix)
     with open(file_out, 'w') as f:
         f.write(str(distance))
-    print(solution, file=sys.stderr)
-    print(distance)
-    pass
+    print(list(map(lambda x: x + 1, solution)), file=sys.stderr)
 
 
 run()
